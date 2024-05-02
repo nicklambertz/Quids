de2ed1482b1bfdf0dbd748bbdab45fb63975b3b2
Jonas Perschke <jonnyp03@icloud.com>
2024-05-02 12:37:01 +0200

    Löschen unnötiget Files

---
+++ quids/src/components/navbar.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    state = { } 
+    render() { 
+        return < nav >navigation</nav>;
+    }
+}
+ 
+export default Navbar;
