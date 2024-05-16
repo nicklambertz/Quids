486c62e8f7af6e558d4faa6d6f6f040120f47925
nicklambertz <n.lambertz@gmx.net>
2024-05-16 16:04:27 +0200

    Setup main page

--- quids/src/App.jsx
+++ quids/src/App.jsx
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import Navbar from './components/navbar';
+import Header from './components/header';
+import Quiz_Main from './components/quiz_main';
 
 class App extends Component {
     state = {  } 
     render() { 
         return <React.Fragment>
-                
+                <div className='header-container'>
+                    <Header/>
+                </div>
+                <div className='quiz-main'>
+                    <Quiz_Main/>
+                </div>
                 <div className='navbar-container'>
-                <Navbar/>
+                    <Navbar/>
                 </div>
         </React.Fragment>;
     }
---
+++ quids/src/components/quiz_main.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Quiz_Main extends Component {
+    state = {  } 
+    render() { 
+        return ;
+    }
+}
+ 
+export default Quiz_Main;
