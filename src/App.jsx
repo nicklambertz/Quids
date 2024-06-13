b579321bd381636a9c1bd09036ee7aa4e37661a7
nicklambertz <n.lambertz@gmx.net>
2024-06-13 11:39:18 +0200

    Install router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import QuizMain from "./components/QuizMain";
